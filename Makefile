2737b320e6b3bfe2e4e4e2c3f885b7f05f42fba7
z5112968 <z5112968@ad.unsw.edu.au>
2020-11-23 01:01:35 +1100

    make and script

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 setup: 
-    ./setup.sh
+	./setup.sh
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-sudo apt-get update &
-sudo apt install -y python3-pip &
+sudo apt-get update
+sudo apt install -y python3-pip
 pip3 install boto3
