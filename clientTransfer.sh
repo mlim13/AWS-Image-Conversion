98fe8ac044086c61a0a2c14e7fb7e105fca092a5
z5112968 <z5112968@ad.unsw.edu.au>
2020-11-24 22:11:42 +1100

    removed redundant credentials

--- clientTransfer.sh
+++ clientTransfer.sh
@@ -1,14 +1,14 @@
 # Setup
-sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./minimoto_setup ubuntu@100.25.137.231:~/minimoto_setup
-sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./minimoto_client ubuntu@100.25.137.231:~/minimoto_client
-sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./minimoto_transcoding ubuntu@100.25.137.231:~/minimoto_transcoding
-sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./minimoto_cleanup ubuntu@100.25.137.231:~/minimoto_cleanup
-sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./minimoto_watchdog ubuntu@100.25.137.231:~/minimoto_watchdog
-sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./Makefile ubuntu@100.25.137.231:~/Makefile
-sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./setup.sh ubuntu@100.25.137.231:~/setup.sh
-sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./img2video.sh ubuntu@100.25.137.231:~/img2video.sh
-sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./mykey.pem ubuntu@100.25.137.231:~/mykey.pem
+sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./minimoto_setup ubuntu@3.236.240.56:~/minimoto_setup
+sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./minimoto_client ubuntu@3.236.240.56:~/minimoto_client
+sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./minimoto_transcoding ubuntu@3.236.240.56:~/minimoto_transcoding
+sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./minimoto_cleanup ubuntu@3.236.240.56:~/minimoto_cleanup
+sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./minimoto_watchdog ubuntu@3.236.240.56:~/minimoto_watchdog
+sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./Makefile ubuntu@3.236.240.56:~/Makefile
+sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./setup.sh ubuntu@3.236.240.56:~/setup.sh
+sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./img2video.sh ubuntu@3.236.240.56:~/img2video.sh
+sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./mykey.pem ubuntu@3.236.240.56:~/mykey.pem
 
-# Client
-sudo scp -r -i mykey.pem -o StrictHostKeyChecking=no ./sample_images2 ubuntu@54.196.200.156:~/sample_images2
-sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./parallelJobs.sh ubuntu@54.196.200.156:~/parallelJobs.sh
+# # Client
+# sudo scp -r -i mykey.pem -o StrictHostKeyChecking=no ./sample_images2 ubuntu@54.196.200.156:~/sample_images2
+# sudo scp -i mykey.pem -o StrictHostKeyChecking=no ./parallelJobs.sh ubuntu@54.196.200.156:~/parallelJobs.sh
